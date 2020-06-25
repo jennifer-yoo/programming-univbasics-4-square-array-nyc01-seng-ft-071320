7017149afcc2ef8be9f118598506066da99606e6
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-25 04:10:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 
 def square_array(array)
   new_array = []
-  array.length.times do |integer|
+  array.length.times { |integer| integer+1 }
     new_array.push(integer * integer)
     end
   new_array
