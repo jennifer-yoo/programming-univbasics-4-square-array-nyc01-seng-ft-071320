8da277856632a1d70fd068b5bda1341abe6e85b0
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-25 04:08:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ require 'pry'
 def square_array(array)
   new_array = []
   array.length.times do |integer|
-    new_array.push(integer.to_i * integer.to_i)
+    new_array.push(integer * integer)
     binding.pry
   end
   new_array
