9dea09f532f103b45141effca4e1d4e74a346fe9
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-25 04:22:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,8 @@
 def square_array(array)
   new_array = []
   count = 0
-  length = array.length
-
   while array[0] do
-    length.times { |integer| }
-    array.push(new_integer * new_integer)
+    array.push((new_integer+1) * (new_integer+1))
     count += 1
         end
     end
